c415a0aee371c818bd761d24ecd677c869b11702
Florian Schnell <floschnell@gmail.com>
2023-11-26 13:27:30 +0100

    improve deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -899,11 +899,13 @@ function App() {
               </Button>
               : null}
             <div style={{ flexGrow: 1 }}></div>
-            <div style={{ display: 'flex', justifyContent: 'center', marginBottom: 0 }}>
+            <div style={{ display: 'flex', justifyContent: 'center', marginBottom: 5 }}>
               <FormControlLabel control={<Switch value={showMunichways} onChange={(_, checked) => setShowMunichways(checked)} />} label="MunichWays Bewertungen" />
             </div>
-            <div style={{ margin: "0 auto 15px auto" }}>
-              <Link style={{ fontSize: "0.7rem", cursor: "pointer" }} onClick={() => setShowImpressum(true)}>Impressum und Datenschutzerklärung</Link>
+            <div style={{fontSize: '0.75rem', margin: "0 auto 15px auto", textAlign: 'center' }}>
+              <Link style={{cursor: "pointer" }} onClick={() => setShowImpressum(true)}>Impressum und Datenschutzerklärung</Link><br />
+              <div style={{ padding: 2 }}></div>
+              <span><b>RadlNavi {process.env.VERSION || "v1"}</b> von </span><Link style={{cursor: "pointer" }} onClick={() => window.open("https://github.com/MunichWays/radlnavi", "_blank")}>https://github.com/MunichWays/radlnavi</Link>
             </div>
           </div>
         </SwipeableDrawer>
